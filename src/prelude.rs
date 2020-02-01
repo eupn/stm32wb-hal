527e5a300bf7495a5a0e5e282f89e4883ad1a445
Evgenii P <eupn@protonmail.com>
2020-02-02 00:07:03 +0800

    Add prelude

---
+++ src/prelude.rs
@@ -0,0 +1,12 @@
+//! Prelude - Include traits for hal
+
+pub use crate::hal::prelude::*; // embedded hal traits
+
+pub use embedded_hal::digital::v2::OutputPin;
+
+pub use crate::datetime::U32Ext as _stm32wb_hal_datetime_U32Ext;
+//pub use crate::dma::DmaExt as _stm32wb_hal_DmaExt;
+//pub use crate::flash::FlashExt as _stm32wb_hal_FlashExt;
+pub use crate::gpio::GpioExt as _stm32wb_hal_GpioExt;
+pub use crate::rcc::RccExt as _stm32wb_hal_RccExt;
+pub use crate::time::U32Ext as _stm32wb_hal_time_U32Ext;
