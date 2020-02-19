e49142e2a9e8b4d397f0616e608d2aa49e3c1cf6
Evgenii P <eupn@protonmail.com>
2020-02-20 03:09:55 +0800

    const.rs: add TryFrom for TlPacketType

--- src/tl_mbox/consts.rs
+++ src/tl_mbox/consts.rs
@@ -1,4 +1,7 @@
+use core::convert::TryFrom;
 
+#[derive(Debug)]
+#[repr(C)]
 pub enum TlPacketType {
     BleCmd = 0x01,
     AclData = 0x02,
@@ -21,4 +24,32 @@ pub enum TlPacketType {
 
     TracesApp = 0x40,
     TracesWl = 0x41,
+}
+
+impl TryFrom<u8> for TlPacketType {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x01 => Ok(TlPacketType::BleCmd),
+            0x02 => Ok(TlPacketType::AclData),
+            0x04 => Ok(TlPacketType::BleEvt),
+            0x08 => Ok(TlPacketType::OtCmd),
+            0x09 => Ok(TlPacketType::OtRsp),
+            0x0A => Ok(TlPacketType::CliCmd),
+            0x0C => Ok(TlPacketType::OtNot),
+            0x0D => Ok(TlPacketType::OtAck),
+            0x0E => Ok(TlPacketType::CliNot),
+            0x0F => Ok(TlPacketType::CliAck),
+            0x10 => Ok(TlPacketType::SysCmd),
+            0x11 => Ok(TlPacketType::SysRsp),
+            0x12 => Ok(TlPacketType::SysEvt),
+            0x20 => Ok(TlPacketType::LocCmd),
+            0x21 => Ok(TlPacketType::LocRsp),
+            0x40 => Ok(TlPacketType::TracesApp),
+            0x41 => Ok(TlPacketType::TracesWl),
+
+            _ => Err(())
+        }
+    }
 }
