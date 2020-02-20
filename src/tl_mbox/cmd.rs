ac9f445e8189c2d2bdf8395ee3725bbc211bb203
Evgenii P <eupn@protonmail.com>
2020-02-20 23:29:33 +0800

    cmd.rs: implement writing CmdPacket to the buffer

--- src/tl_mbox/cmd.rs
+++ src/tl_mbox/cmd.rs
@@ -1,5 +1,6 @@
-use crate::tl_mbox::PacketHeader;
+use crate::tl_mbox::{PacketHeader, TL_EVT_HEADER_SIZE};
 use core::fmt::{Error, Formatter};
+use crate::tl_mbox::evt::{EvtSerial, EvtPacket};
 
 #[derive(Copy, Clone)]
 #[repr(C, packed)]
@@ -7,6 +8,16 @@ pub struct Cmd {
     pub cmd_code: u16,
     pub payload_len: u8,
     pub payload: [u8; 255],
+}
+
+impl Default for Cmd {
+    fn default() -> Self {
+        Self {
+            cmd_code: 0,
+            payload_len: 0,
+            payload: [0u8; 255]
+        }
+    }
 }
 
 impl core::fmt::Debug for Cmd {
@@ -21,18 +32,44 @@ impl core::fmt::Debug for Cmd {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Default)]
 #[repr(C, packed)]
 pub struct CmdSerial {
     pub ty: u8,
     pub cmd: Cmd,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Default)]
 #[repr(C, packed)]
 pub struct CmdPacket {
     pub header: PacketHeader,
     pub cmdserial: CmdSerial,
+}
+
+impl CmdPacket {
+    /// Writes an underlying CmdPacket into the provided buffer.
+    /// Returns a number of bytes that were written.
+    /// Returns an error if event kind is unknown or if provided buffer size is not enough.
+    pub fn write(&self, buf: &mut [u8]) -> Result<usize, ()> {
+        unsafe {
+            let cmd_ptr: *const CmdPacket = self;
+            let self_as_evt_ptr: *const EvtPacket = cmd_ptr.cast();
+            let evt_serial: *const EvtSerial = &(*self_as_evt_ptr).evt_serial;
+
+            let acl_data: *const AclDataPacket = cmd_ptr.cast();
+            let acl_serial: *const AclDataSerial = &(*acl_data).acl_data_serial;
+            let acl_serial_buf: *const u8 = acl_serial.cast();
+
+            let len = (*evt_serial).evt.payload_len as usize + TL_EVT_HEADER_SIZE;
+            if len > buf.len() {
+                return Err(());
+            }
+
+            core::ptr::copy(acl_serial_buf, buf.as_mut_ptr(), len);
+
+            Ok(len)
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
