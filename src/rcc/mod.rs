1f4230757362003172dbacd6bc7acd303469133c
Karl Palsson <karlp@etactica.com>
2021-09-06 11:11:25 +0000

    rcc: fix HSE for sysclock.
    HSE wasn't actually being turned on, so even though the SW bits were set
    to HSE, the SWS bits never switched over, and we continued running from
    MSI.
    
    Fixes: https://github.com/eupn/stm32wb-hal/issues/16
    
    Signed-off-by: Karl Palsson <karlp@etactica.com>

--- src/rcc/mod.rs
+++ src/rcc/mod.rs
@@ -51,12 +51,17 @@ impl Rcc {
             SysClkSrc::Msi(_msi_range) => todo!(),
             SysClkSrc::Hsi => todo!(),
             SysClkSrc::HseSys(hse_div) => {
-                self.clocks.hse = Some(HSE_FREQ.hz());
-
-                self.clocks.sysclk = match hse_div {
-                    HseDivider::NotDivided => HSE_FREQ.hz(),
-                    HseDivider::Div2 => (HSE_FREQ / 2).hz(),
+                // Actually turn on and use HSE....
+                let (divided, f_input) = match hse_div {
+                    HseDivider::NotDivided => (false, HSE_FREQ),
+                    HseDivider::Div2 => (true, HSE_FREQ / 2),
                 };
+                self.rb.cr.modify(|_, w| w.hsepre().bit(divided).hseon().set_bit());
+                // Wait for HSE startup
+                while !self.rb.cr.read().hserdy().bit_is_set() {}
+
+                self.clocks.hse = Some(HSE_FREQ.hz());
+                self.clocks.sysclk = f_input.hz();
 
                 0b10
             }
